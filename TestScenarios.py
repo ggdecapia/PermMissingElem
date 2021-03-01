f96a8196f92eb4e813cff0db5e75e52d8778816f
Glenda Decapia <ggdecapia@yahoo.com>
2021-03-01 10:53:32 -0600

    added description to the scenarios

--- TestScenarios.py
+++ TestScenarios.py
@@ -1,7 +1,13 @@
 # Test Inputs
+#test_when_one_is_missing
 [2, 3, 4, 5]
+#test_when_nothing_is_missing
 [2, 3, 4, 1]
+#test_when_one_is_the_only_element
 [1]
+#test_when_two_is_the_only_element
 [2]
+#test_when_zero_is_the_only_element
 [0]
+#test_when_array_is_empty
 []
